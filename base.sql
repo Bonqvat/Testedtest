025dd758c21e96577a518afed026f42bf28db112
Корыч <es.89020083947@gmail.com>
2025-06-19 03:07:14 +0400

    добавил файл базы

---
+++ base.sql
@@ -0,0 +1,28 @@
+CREATE TABLE cars (
+    id SERIAL PRIMARY KEY,
+    brand VARCHAR(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    year INTEGER NOT NULL,
+    price INTEGER NOT NULL,
+    description TEXT,
+    type VARCHAR(20),
+    features JSONB,
+    images JSONB,
+    specs JSONB,
+    body_type VARCHAR(20),
+    drive VARCHAR(20),
+    power INTEGER,
+    status VARCHAR(20) DEFAULT 'Новый',
+    is_recommended BOOLEAN DEFAULT false,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE feedback (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    subject VARCHAR(100) NOT NULL,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
